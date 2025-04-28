aca1c70cf88f36cfb9a132c0a01ea87894358f1b
Yashin Al Fauzy <yashinfauzy@apps.ipb.ac.id>
2025-04-28 22:58:46 +0700

    yep deployed yep

--- config/database.go
+++ config/database.go
@@ -8,14 +8,25 @@ import (
 
 var DB *gorm.DB
 
+// func ConnectDB() {
+//     dsn := "host=localhost user=postgres password=123 dbname=revabajuanak port=5433 sslmode=disable TimeZone=Asia/Jakarta"
+//     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+//     if err != nil {
+//         panic("Gagal konek ke database")
+//     }
+
+//     DB = db
+//     fmt.Println("Berhasil konek ke database!")
+// }
+
 func ConnectDB() {
-    dsn := "host=localhost user=postgres password=123 dbname=revabajuanak port=5433 sslmode=disable TimeZone=Asia/Jakarta"
+    dsn := "host=ep-wandering-pine-a1atgr3h-pooler.ap-southeast-1.aws.neon.tech user=neondb_owner password=npg_hBS4VIZa7FyU dbname=neondb port=5432 sslmode=require TimeZone=Asia/Jakarta"
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
-        panic("Gagal konek ke database")
+        panic("Failed to connect to the database")
     }
 
     DB = db
-    fmt.Println("Berhasil konek ke database!")
+    fmt.Println("Successfully connected to the database!")
 }
 
